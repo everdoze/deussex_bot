0be3fd1677e63d5e3f087d840a8a419004bdeb79
Andrey Aulov <a.aulov@fast-report.com>
2024-10-08 23:00:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const CHAT_ID = '-1002391201978';
+const API_KEY = '7727226213:AAEuejpfvOf5BxPNLq0SXMyF98WlCcPbUyw';
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+    console.log('Полученные данные:', req.body);
+    res.status(200).send('OK');
+});
+
+app.listen(80, () => {
+    console.log('Прокси-сервер запущен на порту 80');
+});
